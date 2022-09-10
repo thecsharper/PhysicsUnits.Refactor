2d7fdbb86a6e870f8dfae6d6b615ca056ca05c0b
thecsharper <si@thecsharper.com>
2022-09-10 16:49:38 +0100

    Added Electricity and Magnetism tests

---
+++ UnitTests/ElectricityAndMagnetism/ResistanceTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Xunit;
+
+using KMB.Library.Units;
+using KMB.Library.Units.Metric;
+
+namespace UnitTests.ElectricityAndMagnetism
+{
+    [Trait("Category", "Resistance")]
+    public class ResistanceTests : TestBase
+    {
+        [Fact]
+        public void Resistance_Tests()
+        {
+            // A 1 ohm resistor has a voltage drop from 100V to 10V across it.
+            // What is the current through the resistor? 
+            Resistance r = 1.Ohms();
+            ElectricPotential v = 100.Volts() - 10.Volts();
+            Current i = v / r;  // i = 90 amp
+            TestString("i", 90.0, "90 amp", i);
+            
+            Power w = v * i;    // w = 8.1 kW 
+            TestString("w", 8100, "8.1 kW", w);
+
+            // A 10 ohm resistor in a circuit subject to a current of 2 Ampere and a voltage of 120V.
+            // What is the voltage drop across the resistor?
+            r = 10.Ohms();
+            i = 2.Ampere();
+            v = i * r;
+            
+            TestString("v", 20, "20 volt", v);
+        }
+    }
+}
---
+++ UnitTests/ElectricityAndMagnetism/ResistivityTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+using KMB.Library.Units;
+using KMB.Library.Units.Metric;
+
+namespace UnitTests.ElectricityAndMagnetism
+{
+    [Trait("Category", "Resistivity")]
+    public class ResistivityTests : TestBase
+    {
+        [Fact]
+        public void Resistivity_Tests()
+        {
+            // A Nichrome heating element must generate 450 Watts of heat from a 250 Volt Supply.
+            // The heating element is a ribbon 1 mm by 0.05 mm. What length should it be?
+            Power p = 450.Watts();
+            ElectricPotential v = 250.Volts();
+            Area a = 1.Millimetres() * 0.05.Millimetres();
+            Resistivity ρ = 1.1e-6.OhmMetres();
+            // p = v²/r therefore r = v²/p;
+            Resistance r = v.Squared() / p;
+            // r = ρ l/a (Pouillet's law), therefore l = r a / ρ 
+            Length l = r * a / ρ;
+            TestString("l", 6.31313131, "6.31313131 m", l);
+        }
+    }
+}
